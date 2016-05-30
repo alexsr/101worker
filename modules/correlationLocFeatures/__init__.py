520a5e72bcb612da585ec203aa2f579f4d7ae808
Benedikt Kraus <bkraus@uni-koblenz.de>
2016-05-30 16:52:22 +0200

    Add unittest structure

--- modules/correlationLocFeatures/__init__.py
+++ modules/correlationLocFeatures/__init__.py
@@ -6,4 +6,18 @@ config = {
 
 def run(context):
 
+import unittest
+from unittest.mock import Mock, patch
+
+class CorrelationLocFeatures(unittest.TestCase):
+
+	def setUp(self):
+        self.env = Mock()
+        pass
+
+	def test_run(self):
+		pass
+
 def test():
+    suite = unittest.TestLoader().loadTestsFromTestCase(CorrelationLocFeatures)
+    unittest.TextTestRunner(verbosity=2).run(suite)
