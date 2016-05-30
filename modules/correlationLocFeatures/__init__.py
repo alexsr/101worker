cfb01145f8a4918d180fe16e1edcade48cb26879
Benedikt Kraus <bkraus@uni-koblenz.de>
2016-05-30 17:21:44 +0200

    Add write dump

--- modules/correlationLocFeatures/__init__.py
+++ modules/correlationLocFeatures/__init__.py
@@ -1,25 +1,27 @@
 config = {
-	'wantdiff': False,
-	'wantsfiles': False,
-	'threadsafe': True
+    'wantdiff': False,
+    'wantsfiles': False,
+    'threadsafe': True
 }
 
 def run(context):
-  locPerContribution = context.read_dump('locPerContribution')
-  pages = context.read_dump('pages')
+    locPerContribution = context.read_dump('locPerContribution')
+    pages = context.read_dump('pages')
 
+    correlation = {}
+    env.write_dump('CorrelationLocFeatures', correlation)
 
 import unittest
 from unittest.mock import Mock, patch
 
 class CorrelationLocFeatures(unittest.TestCase):
 
-	def setUp(self):
+    def setUp(self):
         self.env = Mock()
         pass
 
-	def test_run(self):
-		pass
+    def test_run(self):
+        pass
 
 def test():
     suite = unittest.TestLoader().loadTestsFromTestCase(CorrelationLocFeatures)
