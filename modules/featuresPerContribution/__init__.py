098c2b483ab63e9b6e029127846de146543be9ad
Anna Schneider <anschneider@uni-koblenz.de>
2016-06-06 16:11:51 +0200

    get features list

--- modules/featuresPerContribution/__init__.py
+++ modules/featuresPerContribution/__init__.py
@@ -7,13 +7,33 @@ config = {
 def run(context):
     pages = context.read_dump('pages')
 
+    if pages is not None and "pages" in pages.keys():
+        pages = pages["pages"]
+    else:
+        pages = []
 
 
 
+    features = []
+    for item in pages:
+        if "namespace" in item.keys() and item["namespace"] == "Feature":
+           if "title" in item.keys() and "used_links" in item.keys():
+                title = item["title"]
+                features += [title]
+    print(features)
+
+
+
+
+
+import unittest
+from unittest.mock import Mock, patch
 
 class FeaturesPerContribution(unittest.TestCase):
 
     def setUp(self):
+    	self.env=Mock()
+    	self.env.read_dump.return_value={}
 
 
 
