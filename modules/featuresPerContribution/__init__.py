31daffbfad13c1f6333f8d1a34d78cbcaba68ed3
Anna Schneider <anschneider@uni-koblenz.de>
2016-06-06 16:39:03 +0200

    add test data

--- modules/featuresPerContribution/__init__.py
+++ modules/featuresPerContribution/__init__.py
@@ -22,7 +22,7 @@ def run(context):
     features = []
     for item in pages:
         if "namespace" in item.keys() and item["namespace"] == "Feature":
-           if "title" in item.keys() and "used_links" in item.keys():
+           if "title" in item.keys():
                 title = item["title"]
                 features += [title]
 
@@ -46,6 +46,59 @@ class FeaturesPerContribution(unittest.TestCase):
     def setUp(self):
     	self.env=Mock()
     	self.env.read_dump.return_value={
+             "pages":[
+    			{
+    				"namespace": "Feature", 
+                	"title": "Hierarchical company", 
+    			},	
+    			{	"namespace": "Feature",
+    				"title": "Mapping"
+    			},
+    			{
+    				"namespace": "Feature", 
+                	"title": "Parsing", 
+    			},
+    			{
+    				"namespace": "Feature", 
+                	"title": "Total", 
+    			},
+    			{
+    				"namespace": "Feature", 
+                	"title": "Cut", 
+    			},
+    			{
+                    "namespace": "Contribution",
+                    "title": "javaJson",
+                     "used_links": [
+                         "Language:JSON",
+                         "Language:Java",
+                         "Technology:javax.json",
+                         "API",
+                         "Contribution:dom",
+                         "Contribution:jdom",
+                         "Language:JSON",
+                         "Language:XML",
+                         "Contribution:javaJsonHttp",
+                         "Technology:Gradle",
+                         "Technology:Eclipse",
+                         "Implements::Feature:Hierarchical company",
+                         "Implements::Feature:Mapping",
+                         "Implements::Feature:Parsing",
+                         "Implements::Feature:Total",
+                         "Implements::Feature:Cut",
+                         "MemberOf::Theme:Java mapping",
+                         "Uses::Language:Java",
+                         "Uses::Language:JSON",
+                         "Uses::Technology:javax.json",
+                         "Uses::Technology:JUnit",
+                         "Uses::Technology:Gradle",
+                         "DevelopedBy::Contributor:rlaemmel"
+                     ]
+                }
+
+
+
+            ], 
     	}
 
     def test_pages(self):
