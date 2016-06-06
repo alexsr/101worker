5895c1fca3f3a3fdcb44a054501eff46214639e1
Benedikt Kraus <bkraus@uni-koblenz.de>
2016-06-06 17:26:48 +0200

    Implement final version of correlationLocFeatures

--- modules/correlationLocFeatures/__init__.py
+++ modules/correlationLocFeatures/__init__.py
@@ -5,33 +5,31 @@ config = {
 }
 
 def run(context):
-    locPerContribution = context.read_dump('locPerContribution')
-    loc_pc_pl = context.read_dump('locPerLanguagePerContribution')
+    features_per_contribution = context.read_dump('featuresPerContribution')
+    loc_pl_pc = context.read_dump('locPerLanguagePerContribution')
     plang_per_contrib = context.read_dump('programmingLanguagePerContribution')
-    pages = context.read_dump('pages')
 
-    if locPerContribution is None:
-        locPerContribution = {}
-
-    if pages is not None and "pages" in pages.keys():
-        pages = pages["pages"]
-    else:
-        pages = []
-
-    contributions = {}
-    for item in pages:
-        if "namespace" in item.keys():
-            if item["namespace"] == "Contribution" and "title" in item.keys() and "used_links" in item.keys():
-                contributions[item["title"]] = item["used_links"]
+    if features_per_contribution is None:
+        features_per_contribution = {}
+    if loc_pl_pc is None:
+        loc_pl_pc = {}
+    if plang_per_contrib is None:
+        plang_per_contrib = {}
 
     locs_per_language = {}
-    for c, lang_locs in loc_pc_pl.items():
+    features_per_language = {}
+    for c, lang_locs in loc_pl_pc.items():
         for l in plang_per_contrib[c]:
             if l not in locs_per_language:
                 locs_per_language[l] = 0
             locs_per_language[l] += lang_locs[l]
+            if l not in features_per_language:
+                features_per_language[l] = []
+            features_per_language[l] = list(set(features_per_contribution[c]) | set(features_per_language[l]))
 
     correlation = {}
+    for l in locs_per_language:
+        correlation[l] = locs_per_language[l] / len(features_per_language[l])
     context.write_dump('correlationLocFeatures', correlation)
 
 import unittest
