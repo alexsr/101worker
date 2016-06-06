f4889786972afcfa7903e793f3366b12c75e193e
Anna Schneider <anschneider@uni-koblenz.de>
2016-06-06 15:55:37 +0200

    add featursPerContribution module

---
+++ modules/featuresPerContribution/__init__.py
@@ -0,0 +1,23 @@
+config = {
+    'wantdiff': False,
+    'wantsfiles': False,
+    'threadsafe': True,
+}
+
+def run(context):
+    pages = context.read_dump('pages')
+
+
+
+
+
+class FeaturesPerContribution(unittest.TestCase):
+
+    def setUp(self):
+
+
+
+
+def test():
+    suite = unittest.TestLoader().loadTestsFromTestCase(FeaturesPerContribution)
+    unittest.TextTestRunner(verbosity=2).run(suite)
